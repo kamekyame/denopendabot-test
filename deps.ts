b375be12836f6334c86edf86c5349ed80c5097ca
denopendabot <denopendabot@github.com>
2023-01-07 00:44:49 +0900

    build(deps): bump std from 0.169.0 to 0.171.0

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-import * as std from "https://deno.land/std@0.169.0/dotenv/mod.ts";
+import * as std from "https://deno.land/std@0.171.0/dotenv/mod.ts";
 import * as client from "https://deno.land/x/twitter_api_client@v0.3.1/mod.ts";
