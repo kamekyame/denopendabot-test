202f9e43d941b68362f17a98c24dc3fe7d1b5e82
denopendabot <denopendabot@github.com>
2023-01-07 00:44:51 +0900

    build(deps): bump twitter_api_client from v0.3.1 to v0.3.2

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
 import * as std from "https://deno.land/std@0.171.0/dotenv/mod.ts";
-import * as client from "https://deno.land/x/twitter_api_client@v0.3.1/mod.ts";
+import * as client from "https://deno.land/x/twitter_api_client@v0.3.2/mod.ts";
